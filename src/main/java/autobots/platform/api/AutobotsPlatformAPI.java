09ba955db0e2564e55dfd4bd0e9957c2cb178649
Matthew Davis <matthew@matthewdavis.io>
2019-05-23 03:39:32 -0500

    first commit

---
+++ src/main/java/autobots/platform/api/AutobotsPlatformAPI.java
@@ -0,0 +1,15 @@
+package autobots.platform.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutobotsPlatformAPI {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(AutobotsPlatformAPI.class, args);
+
+    }
+
+}
---
+++ src/main/java/autobots/platform/api/modules/Module.java
@@ -0,0 +1,25 @@
+package autobots.platform.api.modules;
+
+import lombok.Data;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.validator.constraints.Range;
+
+import javax.persistence.*;
+import java.util.UUID;
+
+@Data
+@Entity
+@Table(name = "modules")
+public class Module {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO, generator = "native")
+    @GenericGenerator(name = "native", strategy = "native")
+    @Range(max = 4294967295L)
+    @Column(nullable = false, updatable = false, unique = true)
+    private Long id;
+
+    @Column(name = "uuid", updatable = false, nullable = false, unique = true, columnDefinition = "BINARY(16)")
+    private UUID uuid;
+
+}
